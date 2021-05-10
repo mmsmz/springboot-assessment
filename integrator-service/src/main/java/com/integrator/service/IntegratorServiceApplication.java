d2eee3e97567118273fed3afccbdf7334d93072c
mohamed <marafath07@gmail.com>
2021-05-10 19:45:49 +0530

    implementing the integrator service

---
+++ integrator-service/src/main/java/com/integrator/service/IntegratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.integrator.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratorServiceApplication.class, args);
+	}
+
+}
---
+++ integrator-service/src/test/java/com/integrator/service/IntegratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrator.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
